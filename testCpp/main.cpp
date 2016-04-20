3b103515ab816c3a3014cdd6a4de39f6541e2cb9
Tiny <tinysilk@hotmail.com>
2016-04-20 14:32:00 +0800

    Initial Commit

---
+++ testCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testCpp
+//
+//  Created by Tiny on 16/4/20.
+//  Copyright © 2016年 Tiny. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
